2d5f9c0ee54bd0bd7ed7eb4fefc3c7553f334a96
Justin Holzmann <jpholzmann@gmail.com>
2023-10-27 22:11:10 -0500

    LLM is working in iex.

--- config/config.exs
+++ config/config.exs
@@ -61,6 +61,16 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+
+config :ex_openai,
+  # find it at https://platform.openai.com/account/api-keys
+  api_key: System.get_env("OPENAI_API_KEY"),
+  # find it at https://platform.openai.com/account/api-keys
+  organization_key: System.get_env("OPENAI_ORGANIZATION_KEY"),
+  # optional, passed to [HTTPoison.Request](https://hexdocs.pm/httpoison/HTTPoison.Request.html) options
+  http_options: [recv_timeout: 50_000]
+
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
